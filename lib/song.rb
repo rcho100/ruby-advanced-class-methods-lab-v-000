14495871e555ef446e70411bb28f9a0201a59265
Richard Cho <richardcho100@gmail.com>
2018-05-11 03:27:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
 
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) == nil
-      song = self.create_by_name(name)
+      self.create_by_name(name)
     else
       self.find_by_name(name)
     end
